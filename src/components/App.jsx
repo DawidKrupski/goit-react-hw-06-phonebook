e0214eb13131121a8717aadb9454779d341d8262
DawidKrupski <dawid.krupski323@gmail.com>
2023-03-18 08:37:23 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { AddContact } from './AddContact/AddContact';
 import { Filter } from './ContactList/Filter/Filter';
